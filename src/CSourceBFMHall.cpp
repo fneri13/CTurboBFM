81146d1e1a1bf39a7e812b7b2e876a376177f30c
Francesco Neri <francesconeri95@gmail.com>
2025-05-08 15:20:37 +0200

    added the missing density in force computation

--- src/CSourceBFMHall.cpp
+++ src/CSourceBFMHall.cpp
@@ -5,7 +5,7 @@ StateVector CSourceBFMHall::computeBodyForceSource(size_t i, size_t j, size_t k,
     
     FloatType volume = _mesh.getVolume(i, j, k);
     FloatType forceMagnitude = _relativeVelocityCylindric.dot(_relativeVelocityCylindric) * M_PI * _deviationAngle / _pitch / std::abs(_normalCamberTangential);
-    Vector3D forceCylindrical = _inviscidForceDirection * forceMagnitude;
+    Vector3D forceCylindrical = _inviscidForceDirectionCylindrical * forceMagnitude;
     Vector3D forceCartesian = computeCartesianVectorFromCylindrical(forceCylindrical, _theta);
     
     StateVector source({0,0,0,0,0});
@@ -14,5 +14,5 @@ StateVector CSourceBFMHall::computeBodyForceSource(size_t i, size_t j, size_t k,
     source[3] = forceCartesian.z();
     source[4] = forceCylindrical.z() * _omega * _radius;
 
-    return source*volume;
+    return source*volume*primitive[0];
 }

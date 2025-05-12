36156c5bd69af3b597777e5199f5ee58a79b2fb6
Francesco Neri <francesconeri95@gmail.com>
2025-05-12 12:52:11 +0200

    python script to plot the content of turbo.csv

---
+++ python/Plot_Turbo.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+#### Plot the turbo performance of the simulation in csv format
+
+import numpy as np
+import pandas as pd
+import argparse
+import os
+import matplotlib.pyplot as plt
+
+def read_file(input_filename):
+    df = pd.read_csv(input_filename)
+    data_dict = {col: df[col].to_numpy() for col in df.columns}
+    return data_dict
+
+
+def shift_to_zero(res):
+    return res - res[0]
+
+def plot_turbo(data):
+    
+    # mass flow plot
+    plt.figure()
+    plt.plot(data["Massflow"])
+    plt.grid(alpha=0.3)
+    plt.xlabel("Iteration [-]")
+    plt.ylabel(r"$\dot{m}$ [-]")
+    
+    # beta plot
+    plt.figure()
+    plt.plot(data["PRtt"], label=r"$\beta_{tt}$")
+    plt.plot(data["TRtt"], label=r"$\tau_{tt}$")
+    plt.plot(data["ETAtt"], label=r"$\eta_{tt}$")
+    plt.grid(alpha=0.3)
+    plt.xlabel("Iteration [-]")
+    plt.legend()
+    
+    # beta evolution plot
+    plt.figure()
+    plt.plot(data["Massflow"], data["PRtt"], '-k', lw=0.5)
+    plt.scatter(data["Massflow"], data["PRtt"], c=np.linspace(1, len(data["Massflow"]), len(data["Massflow"])))
+    plt.grid(alpha=0.3)
+    plt.xlabel(r"$\dot{m}$ [-]")
+    plt.ylabel(r"$\beta_{tt}$ [-]")
+    plt.colorbar()
+    
+    # eta evolution plot
+    plt.figure()
+    plt.plot(data["Massflow"], data["ETAtt"], '-k', lw=0.5)
+    plt.scatter(data["Massflow"], data["ETAtt"], c=np.linspace(1, len(data["Massflow"]), len(data["Massflow"])))
+    plt.grid(alpha=0.3)
+    plt.xlabel(r"$\dot{m}$ [-]")
+    plt.ylabel(r"$\eta_{tt}$ [-]")
+    plt.colorbar()
+        
+    plt.show()
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Plot the performance of a CTurboBFMsimulation.")
+    parser.add_argument("input_file", help="Path to the turbo performance CSV file")
+    args = parser.parse_args()
+
+    input_filename = args.input_file
+    
+    data = read_file(input_filename)
+    plot_turbo(data)

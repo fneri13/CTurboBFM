400018dc3022a0f5a5b79e76f41c0181b33a07df
Francesco Neri <francesconeri95@gmail.com>
2025-10-29 13:45:56 +0100

    fixed bug in sorting, and updated

--- python/Plot_MonitorPoints.py
+++ python/Plot_MonitorPoints.py
@@ -7,52 +7,69 @@ import os
 import argparse
 # from styles import *
 
-# --- Argument Parsing ---
-parser = argparse.ArgumentParser(description="Plot monitor point data with a given time per revolution. Use 1 to plot in seconds.")
-parser.add_argument("time_per_revolution", type=float, help="Time per revolution in seconds")
-args = parser.parse_args()
-time_per_revolution = args.time_per_revolution
 
-# --- File Setup ---
-os.makedirs("Pictures", exist_ok=True)
-cwd = os.getcwd()
-files = os.listdir(cwd + '/Monitor_Points')
-inputFiles = sorted([cwd + '/Monitor_Points/' + file for file in files if file.endswith('.csv')])
-
-# --- Read & Process Data ---
-dfs = []
-for inputFile in inputFiles:
-    df = pd.read_csv(inputFile)
-    df["Velocity_Magnitude[m/s]"] = np.sqrt(df["Velocity_X[m/s]"]**2 + df["Velocity_Y[m/s]"]**2 + df["Velocity_Z[m/s]"]**2)
-    dfs.append(df)
-
-# --- Fields to Plot ---
-fieldList = [
-    'Pressure[Pa]',
-    'Velocity_Magnitude[m/s]',
-]
-
-# --- Plotting ---
-for ii, field in enumerate(fieldList):
-    plt.figure()
+def read_monitor_points_data():
     
-    if len(dfs) > 1:
-        for i in range(1, len(dfs)):
-            plt.plot(dfs[i]["Time[s]"]/time_per_revolution, dfs[i][field], label=f"Point {i}")
-    else:
-        plt.plot(dfs[0]["Time[s]"]/time_per_revolution, dfs[0][field])
+    # --- File Setup ---
+    os.makedirs("Pictures", exist_ok=True)
+    cwd = os.getcwd()
+    files = os.listdir(cwd + '/Monitor_Points')
+    inputFiles = sorted([cwd + '/Monitor_Points/' + file for file in files if file.endswith('.csv')])
 
-    if time_per_revolution!=1:
-        plt.xlabel("Revs [-]")
-    else:
-        plt.xlabel("Time [s]") # to give availability to plit in seconds if needed
-    plt.ylabel(field)
-
-    # plt.legend()
-    plt.grid(alpha=0.3)
-    plt.savefig(f"Pictures/monitorPoint_{ii}.pdf", bbox_inches='tight')
+    # --- Read & Process Data ---
+    dfs = []
+    for inputFile in inputFiles:
+        df = pd.read_csv(inputFile)
+        df["Velocity_Magnitude[m/s]"] = np.sqrt(df["Velocity_X[m/s]"]**2 + df["Velocity_Y[m/s]"]**2 + df["Velocity_Z[m/s]"]**2)
+        dfs.append(df)
     
-    
+    return dfs
 
+
+
+def main():
+    # --- Argument Parsing ---
+    parser = argparse.ArgumentParser(description="Plot monitor point data with a given time per revolution. Use 1 to plot in seconds.")
+    parser.add_argument("time_per_revolution", type=float, help="Time per revolution in seconds")
+    args = parser.parse_args()
+    time_per_revolution = args.time_per_revolution
+
+    # read data
+    dfs = read_monitor_points_data()
+
+    # --- Fields to Plot ---
+    fieldList = [
+        'Pressure[Pa]',
+        'Velocity_Magnitude[m/s]',
+    ]
+    
+    fieldNames = [
+        'Pressure',
+        'Velocity',
+    ]
+
+    # --- Plotting ---
+    for ii, field in enumerate(fieldList):
+        plt.figure()
+        
+        if len(dfs) > 1:
+            for i in range(1, len(dfs)):
+                plt.plot(dfs[i]["Time[s]"]/time_per_revolution, dfs[i][field], label=f"Point {i}")
+        else:
+            plt.plot(dfs[0]["Time[s]"]/time_per_revolution, dfs[0][field])
+
+        if time_per_revolution!=1:
+            plt.xlabel("Revs [-]")
+        else:
+            plt.xlabel("Time [s]") # to give availability to plit in seconds if needed
+        plt.ylabel(field)
+
+        # plt.legend()
+        plt.grid(alpha=0.3)
+        plt.savefig(f"Pictures/Monitor_Points_{fieldNames[ii]}.pdf", bbox_inches='tight')
+    
+if __name__ == "__main__":
+    main()
+    
 plt.show()
 
--- python/Plot_VelocitySensors.py
+++ python/Plot_VelocitySensors.py
@@ -7,62 +7,28 @@ import os
 import re
 # from styles import *
 import argparse
-
-os.makedirs("Pictures", exist_ok=True)
-
-# --- Argument Parsing ---
-parser = argparse.ArgumentParser(description="Plot monitor point data with a given time per revolution. Use 1 to plot in seconds.")
-parser.add_argument("time_per_revolution", type=float, help="Time per revolution in seconds")
-args = parser.parse_args()
-time_per_revolution = args.time_per_revolution
-
-def extractOrder(filepath):
-    """Extracts the integer index from filenames like 'monitorPoint_12.csv'."""
-    match = re.search(r'monitorPoint_(\d+)\.csv$', filepath)
-    return int(match.group(1)) if match else -1
-
-cwd = os.getcwd()
-files = os.listdir(cwd + '/Monitor_Points')
-
-inputFiles = [cwd + '/Monitor_Points/' + file for file in files if file.endswith('.csv')]
-inputFiles = sorted(inputFiles, key=extractOrder)
-
-dfs = []
-for inputFile in inputFiles:
-    df = pd.read_csv(inputFile)
-    df["Velocity_Magnitude[m/s]"] = np.sqrt(df["Velocity_X[m/s]"]**2 + df["Velocity_Y[m/s]"]**2 + df["Velocity_Z[m/s]"]**2)
-    dfs.append(df)
-
-pressureSensors = []
-for df in dfs:
-    pressureSensors.append(df["Velocity_Magnitude[m/s]"])
-time = dfs[0]["Time[s]"]
-
-print()
+from Plot_MonitorPoints import read_monitor_points_data
+from Plot_PressureSensors import plot_normalized_sensors_oscillation
 
 
-def scale_oscillations(pressure, deltaTheta, scaling=1.1):
-    y = (pressure-pressure.min())/(pressure.max()-pressure.min())*deltaTheta/scaling
-    y = y-y[0]
-    return y
+def main():
+    # --- Argument Parsing ---
+    parser = argparse.ArgumentParser(description="Plot monitor point data with a given time per revolution. Use 1 to plot in seconds.")
+    parser.add_argument("time_per_revolution", type=float, help="Time per revolution in seconds")
+    args = parser.parse_args()
+    time_per_revolution = args.time_per_revolution
 
-nSensors = len(pressureSensors)
-plt.figure()
-for i in range(0, len(pressureSensors)):
-    deltaTheta = 360 / (nSensors)
-    theta = i * 360 / (nSensors)
-    plt.plot(time/time_per_revolution, scale_oscillations(pressureSensors[i], deltaTheta) + theta, label=f"probe {i}")
+    dfs = read_monitor_points_data()
 
-if time_per_revolution != 1:
-    plt.xlabel("Revs [-]")
-else:
-    plt.xlabel(r"$t$ [s]")
-plt.ylabel(r"$\theta$ [deg]")
-# plt.legend()
-plt.grid(alpha=.3)
-try:
-    plt.savefig("Pictures/VelocitySensors.pdf", bbox_inches='tight')
-except:
-    print("Could not save figure. Pictures folder not existing.")
+    velocitySensors = []
+    for df in dfs:
+        velocitySensors.append(df["Velocity_Magnitude[m/s]"])
+    time = dfs[0]["Time[s]"]
+
+    plot_normalized_sensors_oscillation(velocitySensors, time, time_per_revolution, "Velocity")
+
+
+if __name__ == "__main__":
+    main()
 
 plt.show()

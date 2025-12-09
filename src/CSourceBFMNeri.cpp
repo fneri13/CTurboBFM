f54fe47dc5b5701c2a064d97b35eedae80a58ba8
Francesco Neri <francesconeri95@gmail.com>
2025-12-09 16:27:05 +0100

    fixed, but not working

--- src/CSourceBFMNeri.cpp
+++ src/CSourceBFMNeri.cpp
@@ -9,18 +9,17 @@ CSourceBFMNeri::CSourceBFMNeri( const Config &config, const CFluidBase &fluid, c
 
 StateVector CSourceBFMNeri::computeBodyForceSource( size_t i, size_t j, size_t k, const StateVector& primitive,
                                                     Matrix3D<Vector3D> &inviscidForce, Matrix3D<Vector3D> &viscousForce) {
+    // update flow state
     computeFlowState(i, j, k, primitive);
-
     computeStreamwiseCoefficient(i, j, k);
-                                                    
+    
+    // compute scaling coefficients for turning and loss profiles
     FloatType currentMassFlow = _turboPerformance[TurboPerformance::MASS_FLOW].back();
-    
     _scalingTurning = interpolateLinear(_inputTable.getField(FieldNames::CHIMA_MASS_FLOW), _inputTable.getField(FieldNames::CHIMA_SCALING_TURNING), currentMassFlow);
-    
     _scalingLoss = interpolateLinear(_inputTable.getField(FieldNames::CHIMA_MASS_FLOW), _inputTable.getField(FieldNames::CHIMA_SCALING_LOSS), currentMassFlow);
     
+    // compute body force contributions
     StateVector viscousComponent = computeViscousComponent(i, j, k, primitive, viscousForce);
-
     StateVector inviscidComponent = computeInviscidComponent(i, j, k, primitive, inviscidForce);
     
     return inviscidComponent + viscousComponent;
@@ -29,15 +28,11 @@ StateVector CSourceBFMNeri::computeBodyForceSource( size_t i, size_t j, size_t k
 
 
 void CSourceBFMNeri::computeStreamwiseCoefficient(size_t i, size_t j, size_t k) {
-    
+    // streamwise distribution function taken from Chima article 2006
     FloatType mLocal{0.0};
-
-    mLocal = _mesh.getInputFields(FieldNames::STREAMWISE_LENGTH, i, j, k) + 1e-8;
-
+    mLocal = _mesh.getInputFields(FieldNames::STREAMWISE_LENGTH, i, j, k) + 1e-9;
     _mTrailingEdge = _mesh.getInputFields(FieldNames::STREAMWISE_LENGTH, _trailingEdgeIndex, j, k);
-
     _mLeadingEdge = _mesh.getInputFields(FieldNames::STREAMWISE_LENGTH, _leadingEdgeIndex, j, k);
-
     _streamwiseCoeff = std::sqrt((mLocal-_mLeadingEdge)/(_mTrailingEdge-_mLeadingEdge)) * 0.001; // 0.001 for r4 works
 }
 
@@ -46,42 +41,30 @@ void CSourceBFMNeri::computeStreamwiseCoefficient(size_t i, size_t j, size_t k) 
 
 StateVector CSourceBFMNeri::computeViscousComponent(size_t i, size_t j, size_t k, const StateVector& primitive, Matrix3D<Vector3D> &viscousForce) {
     
+    // compute the magnitude
     FloatType deltaS_deltaM_ref = _mesh.getInputFields(FieldNames::DELTA_ENTROPY_DM, i, j, k);
-    
-    deltaS_deltaM_ref *= _scalingLoss;
-    
+    FloatType deltaS_deltaM = deltaS_deltaM_ref * _scalingLoss;
     FloatType relVelMag = _relativeVelocityCylindric.magnitude();
-
     FloatType temperature = _fluid.computeTemperature_rho_u_et(primitive[0], {primitive[1], primitive[2], primitive[3]}, primitive[4]);
-
     FloatType forceMagOld = viscousForce(i, j, k).magnitude();
+    StateVector conservativeAtTrailEdge = _conservativeSolution.at(_trailingEdgeIndex, j, k);
+    StateVector primitiveAtTrailEdge = getEulerPrimitiveFromConservative(conservativeAtTrailEdge);
+    FloatType entropyAtTrailEdge = _fluid.computeEntropy_rho_u_et(primitiveAtTrailEdge[0], {primitiveAtTrailEdge[1], primitiveAtTrailEdge[2], primitiveAtTrailEdge[3]}, primitiveAtTrailEdge[4]);
+    FloatType deltaS_deltaM_actual = entropyAtTrailEdge / (_mTrailingEdge - _mLeadingEdge);    
 
-    StateVector conservativeTrailEdge = _conservativeSolution.at(_trailingEdgeIndex, j, k);
-
-    StateVector primitiveTrailEdge = getEulerPrimitiveFromConservative(conservativeTrailEdge);
-
-    FloatType entropyTrailEdge = _fluid.computeEntropy_rho_u_et(primitiveTrailEdge[0], {primitiveTrailEdge[1], primitiveTrailEdge[2], primitiveTrailEdge[3]}, primitiveTrailEdge[4]);
+    // compute the new magnitude, assuming proportional control over deltaS/deltaM
+    FloatType forceMag = forceMagOld - (deltaS_deltaM_actual - deltaS_deltaM) / deltaS_deltaM * _streamwiseCoeff;
     
-    FloatType deltaS_deltaM_actual = entropyTrailEdge / (_mTrailingEdge - _mLeadingEdge);    
-    
-    FloatType forceMag = forceMagOld - (deltaS_deltaM_actual - deltaS_deltaM_ref) * temperature * _velMeridional / relVelMag * _streamwiseCoeff;
-
+    // contribution to bfm gov. equations
     Vector3D forceCylindrical = _viscousForceDirectionCylindrical * forceMag;
-    
     _viscousForceCylindrical = forceCylindrical;
-
     Vector3D forceCartesian = computeCartesianVectorFromCylindrical(forceCylindrical, _theta);
-
     viscousForce(i, j, k) = forceCartesian;
     
     StateVector source({0,0,0,0,0});
-    
     source[1] = forceCartesian.x();
-    
     source[2] = forceCartesian.y();
-    
     source[3] = forceCartesian.z();
-    
     source[4] = forceCylindrical.z() * _omega * _radius;
 
     FloatType volume = _mesh.getVolume(i, j, k);
@@ -94,40 +77,29 @@ StateVector CSourceBFMNeri::computeViscousComponent(size_t i, size_t j, size_t k
 
 StateVector CSourceBFMNeri::computeInviscidComponent(size_t i, size_t j, size_t k, const StateVector& primitive, Matrix3D<Vector3D> &inviscidForce) {
     
+    // update the state
     FloatType deltaTotEnthalpy_dm_ref = _mesh.getInputFields(FieldNames::DELTA_TOT_ENTHALPY_DM, i, j, k);
-    
-    deltaTotEnthalpy_dm_ref *= _scalingTurning;
-
+    FloatType deltaTotEnthalpy_dm = deltaTotEnthalpy_dm_ref * _scalingTurning;
     StateVector conservativeTrailEdge = _conservativeSolution.at(_trailingEdgeIndex, j, k);
-
     StateVector primitiveTrailEdge = getEulerPrimitiveFromConservative(conservativeTrailEdge);
-
     FloatType totEnthalpyTrailEdge = _fluid.computeTotalEnthalpy_rho_u_et(primitiveTrailEdge[0], {primitiveTrailEdge[1], primitiveTrailEdge[2], primitiveTrailEdge[3]}, primitiveTrailEdge[4]); 
-
     FloatType deltaTotEnthalpy_dm_actual = (totEnthalpyTrailEdge - 1005.0*288.15) / (_mTrailingEdge - _mLeadingEdge);
 
+    // compute the magnitude
     FloatType tangForceOld = inviscidForce(i, j, k).z() + _viscousForceCylindrical.z();
-    
-    _tangentialForce = tangForceOld - (deltaTotEnthalpy_dm_actual - deltaTotEnthalpy_dm_ref) * _velMeridional / _omega / _radius * _streamwiseCoeff;
-
+    _tangentialForce = tangForceOld - (deltaTotEnthalpy_dm_actual - deltaTotEnthalpy_dm) / deltaTotEnthalpy_dm * _streamwiseCoeff;
     FloatType forceInviscidTangential = _tangentialForce - _viscousForceCylindrical.z();
+    FloatType forceInviscidMagnitude = std::abs(forceInviscidTangential / _inviscidForceDirectionCylindrical.z());
 
-    FloatType forceInviscidMagnitude = forceInviscidTangential / _inviscidForceDirectionCylindrical.z();
-
+    // contribution to flow equations
     Vector3D forceCylindrical = _inviscidForceDirectionCylindrical * forceInviscidMagnitude;
-
     Vector3D forceCartesian = computeCartesianVectorFromCylindrical(forceCylindrical, _theta);
-    
     inviscidForce(i, j, k) = forceCartesian;
     
     StateVector source({0,0,0,0,0});
-    
     source[1] = forceCartesian.x();
-    
     source[2] = forceCartesian.y();
-    
     source[3] = forceCartesian.z();
-    
     source[4] = forceCylindrical.z() * _omega * _radius;
     
     FloatType volume = _mesh.getVolume(i, j, k);

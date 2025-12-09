56a842ed5df182f37dd29940721d1120ca260857
Francesco Neri <francesconeri95@gmail.com>
2025-12-09 16:26:52 +0100

    just rewritten

--- src/CSourceBFMChima.cpp
+++ src/CSourceBFMChima.cpp
@@ -8,16 +8,17 @@ CSourceBFMChima::CSourceBFMChima(const Config &config, const CFluidBase &fluid, 
 
 
 StateVector CSourceBFMChima::computeBodyForceSource(size_t i, size_t j, size_t k, const StateVector& primitive, Matrix3D<Vector3D> &inviscidForce, Matrix3D<Vector3D> &viscousForce) {
+    
+    // Update bfm state variables
     computeFlowState(i, j, k, primitive);
 
+    // compute scaling coefficients
     FloatType currentMassFlow = _turboPerformance[TurboPerformance::MASS_FLOW].back();
-    
     _scalingTurning = interpolateLinear(_inputTable.getField(FieldNames::CHIMA_MASS_FLOW), _inputTable.getField(FieldNames::CHIMA_SCALING_TURNING), currentMassFlow);
-    
     _scalingLoss = interpolateLinear(_inputTable.getField(FieldNames::CHIMA_MASS_FLOW), _inputTable.getField(FieldNames::CHIMA_SCALING_LOSS), currentMassFlow);
     
+    // compute body force contributions
     StateVector viscousComponent = computeViscousComponent(i, j, k, primitive, viscousForce);
-
     StateVector inviscidComponent = computeInviscidComponent(i, j, k, primitive, inviscidForce);
     
     return inviscidComponent + viscousComponent;
@@ -26,32 +27,23 @@ StateVector CSourceBFMChima::computeBodyForceSource(size_t i, size_t j, size_t k
 
 StateVector CSourceBFMChima::computeInviscidComponent(size_t i, size_t j, size_t k, const StateVector& primitive, Matrix3D<Vector3D> &inviscidForce) {
     
-    FloatType deltaTotEnthalpy_dm = _mesh.getInputFields(FieldNames::DELTA_TOT_ENTHALPY_DM, i, j, k);
-    
-    deltaTotEnthalpy_dm *= _scalingTurning;
-    
+    // compute the magnitude
+    FloatType deltaTotEnthalpy_dm_ref = _mesh.getInputFields(FieldNames::DELTA_TOT_ENTHALPY_DM, i, j, k);
+    FloatType deltaTotEnthalpy_dm = deltaTotEnthalpy_dm_ref * _scalingTurning;
     _tangentialForce = deltaTotEnthalpy_dm * _velMeridional /( _radius * _omega);
-
     FloatType forceInviscidTangential = _tangentialForce - _viscousForceCylindrical.z();
+    FloatType forceInviscidMagnitude = std::abs(forceInviscidTangential / _inviscidForceDirectionCylindrical.z());
 
-    FloatType forceInviscidMagnitude = forceInviscidTangential / _inviscidForceDirectionCylindrical.z();
-
+    // contribution to flow equations
     Vector3D forceCylindrical = _inviscidForceDirectionCylindrical * forceInviscidMagnitude;
-
     Vector3D forceCartesian = computeCartesianVectorFromCylindrical(forceCylindrical, _theta);
-    
     inviscidForce(i, j, k) = forceCartesian;
     
     StateVector source({0,0,0,0,0});
-    
     source[1] = forceCartesian.x();
-    
     source[2] = forceCartesian.y();
-    
     source[3] = forceCartesian.z();
-    
     source[4] = forceCylindrical.z() * _omega * _radius;
-    
     FloatType volume = _mesh.getVolume(i, j, k);
 
     return source*volume*primitive[0];
@@ -61,34 +53,25 @@ StateVector CSourceBFMChima::computeInviscidComponent(size_t i, size_t j, size_t
 
 
 StateVector CSourceBFMChima::computeViscousComponent(size_t i, size_t j, size_t k, const StateVector& primitive, Matrix3D<Vector3D> &viscousForce) {
+    
+    // compute the magnitude
     FloatType deltaS_deltaM_ref = _mesh.getInputFields(FieldNames::DELTA_ENTROPY_DM, i, j, k);
-    
-    deltaS_deltaM_ref *= _scalingLoss;
-    
+    FloatType deltaS_deltaM = deltaS_deltaM_ref * _scalingLoss;
     FloatType relVelMag = _relativeVelocityCylindric.magnitude();
-
     FloatType temperature = _fluid.computeTemperature_rho_u_et(primitive[0], {primitive[1], primitive[2], primitive[3]}, primitive[4]);
-    
-    FloatType forceMag = temperature * _velMeridional / relVelMag * deltaS_deltaM_ref;
+    FloatType forceMag = temperature * _velMeridional / relVelMag * deltaS_deltaM;
 
+    // contribution to flow equations
     Vector3D forceCylindrical = _viscousForceDirectionCylindrical * forceMag;
-    
     _viscousForceCylindrical = forceCylindrical;
-
     Vector3D forceCartesian = computeCartesianVectorFromCylindrical(forceCylindrical, _theta);
-
     viscousForce(i, j, k) = forceCartesian;
     
     StateVector source({0,0,0,0,0});
-    
     source[1] = forceCartesian.x();
-    
     source[2] = forceCartesian.y();
-    
     source[3] = forceCartesian.z();
-    
     source[4] = forceCylindrical.z() * _omega * _radius;
-
     FloatType volume = _mesh.getVolume(i, j, k);
 
     return source*volume*primitive[0];
